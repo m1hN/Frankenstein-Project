75a69c41bbef32b1bb63610c6c05bf6ff4a7629c
m1hN <145584972+m1hN@users.noreply.github.com>
2023-12-12 20:19:36 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,115 @@ function showReply() {
     <p><pre>- therealfrankenstein</pre></p> 
     <img src="img/frankenstein-03.webp" alt="franken" width="500"></div> `;
 }
+
+document.getElementById("replybtn1").addEventListener("click", showReply1);
+
+function showReply1() {
+
+    let output = document.getElementById("outputReply1");
+    console.log(output)
+
+    document.getElementById("outputReply1").innerHTML = `<div id="commentsec"> <p>no you idiot. are you mad bud?</p>
+    <pre>- Jack</pre></p>
+    <hr>
+    <p>its a commen misconsception that the monster's name is frankenstein. the monster is just the monster</p>
+                <pre>- wee/woo</pre></p>
+                <hr>`;
+}
+
+document.getElementById("replybtn2").addEventListener("click", showReply2);
+
+function showReply2() {
+
+    let output = document.getElementById("outputReply2");
+    console.log(output)
+
+    document.getElementById("outputReply2").innerHTML = `<div id="commentsec"> 
+    <p>thats a monkey, not the monster</p>
+    <pre>- bacotls2</pre></p>
+    <hr>
+    <p>fr fr</p>
+    <pre>- catlover23</pre></p>
+    <hr>`;
+}
+
+document.getElementById("replybtn3").addEventListener("click", showReply3);
+
+function showReply3() {
+
+    let output = document.getElementById("outputReply3");
+    console.log(output)
+
+    document.getElementById("outputReply3").innerHTML = `<div id="commentsec"> 
+    <p>Victor probably accidently made the monster immortal 0-0</p>
+                <pre>- cloud9</pre></p>
+                <hr>`;
+}
+
+document.getElementById("replybtn4").addEventListener("click", showReply4);
+
+function showReply4() {
+
+    let output = document.getElementById("outputReply4");
+    console.log(output)
+
+    document.getElementById("outputReply4").innerHTML = `<div id="commentsec"> 
+    <p>Yeah they do look similar!</p>
+                <pre>- simpsondathird</pre></p>
+                <hr>
+                <p>yeah bro look at the image they both black</p>
+                <pre>- PropsJohnson</pre></p>
+                <hr>
+                <p>they both look like monkeys</p>
+                <pre>- timothy</pre></p>
+                <hr>
+                <p>the monster in the book hates people right? doesn't it seem like the monster in the image from John Doe is going towards the cameraman?</p>
+                <pre>- DJ</pre></p>
+                <hr>
+                <p>I dont imagine the monster to look like that. and yeah that is more like blackfoot than frankenstein</p>
+                <pre>- PB&J</pre></p>
+                <hr>`;
+}
+
+document.getElementById("replybtn5").addEventListener("click", showReply5);
+
+function showReply5() {
+
+    let output = document.getElementById("outputReply5");
+    console.log(output)
+
+    document.getElementById("outputReply5").innerHTML = `<div id="commentsec"> 
+    <p>Texas be wildin now</p>
+                <pre>- texasRocks!</pre></p>
+                <hr>
+                <img src="https://media0.giphy.com/media/RAcLMuqL3X6ZG/giphy.gif" alt="">
+                <p>people are discussing the image everywhere now!</p>
+                <pre>- wario</pre></p>
+                <hr>
+                <p>is this a serious problem? If it is truly the monster in the book, that means he's going to come after us!</p>
+                <pre>- 89</pre></p>
+                <hr>
+                <p>the monster in the book hates people right?</p>
+                <pre>- JD</pre></p>
+                <hr>
+                <p>ya'll some people are going hunting for the monster. A group called "Monster Busters"</p>
+                <p>people are making it seem so dangerous when its most likely fake</p>
+                <pre>- tsktsktsk</pre></p>
+                <hr>
+                <p>thats just wasted effort</p>
+                <pre>- spiderman</pre></p>
+                <hr>`;
+}
+
+document.getElementById("replybtn6").addEventListener("click", showReply6);
+
+function showReply6() {
+
+    let output = document.getElementById("outputReply6");
+    console.log(output)
+
+    document.getElementById("outputReply6").innerHTML = `<div id="commentsec"> 
+    <p>The monster is going to eat us! Ahhhhhhhhhh!</p>
+                <pre>- outthyere</pre></p>
+                <hr>`;
+}
